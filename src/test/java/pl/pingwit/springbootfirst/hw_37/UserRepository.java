7abda539ed41ce56c254dbb6393323bb459e92a5
Your Name <you@example.com>
2023-06-18 20:20:13 -1100

    lkgszl

---
+++ src/test/java/pl/pingwit/springbootfirst/hw_37/UserRepository.java
@@ -0,0 +1,40 @@
+package pl.pingwit.springbootfirst.hw_37;
+
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Component
+public class UserRepository {
+
+    private final DataSource dataSource;
+
+    public UserRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public List<User> findAllUsers() {
+        List<User> users = new ArrayList<>();
+        try (Connection connection = dataSource.getConnection()) {
+            Statement statement = connection.createStatement();
+
+            ResultSet rs = statement.executeQuery("SELECT * from users");
+
+            while (rs.next()) {
+                User user = new User(rs.getString(2), rs.getString(3));
+                users.add(user);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return users;
+    }
+}
---
+++ src/test/java/pl/pingwit/springbootfirst/hw_37/UserService.java
@@ -0,0 +1,21 @@
+package pl.pingwit.springbootfirst.hw_37;
+
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    public void printUsers() {
+        List<User> allUsers = userRepository.findAllUsers();
+        allUsers.forEach(System.out::println);
+    }
+
+
+}
